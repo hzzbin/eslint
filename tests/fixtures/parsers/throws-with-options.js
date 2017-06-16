hzzbin/eslint

commit afbea78d4b94839ec6d59d2c98321901aeb76313

Chore: don't pull default options from eslint:recommended (fixes #8374) (#8381)
Previously, several internal modules used `conf/eslint-recommended.js` as a source for defaults. This made it confusing to work with `conf/eslint-recommended.js`, because it's ostensibly just a config and it shouldn't have any effect when not extending from `eslint:recommended`.

This commit separates the default configuration into its own file and removes some hardcoded defaults from the source code.

conf/default-config-options.js
@@ -1,0 +1,33 @@
+/**
+ * @fileoverview Default config options
+ * @author Teddy Katz
+ */
+
+"use strict";
+
+/**
+ * Freezes an object and all its nested properties
+ * @param {Object} obj The object to deeply freeze
+ * @returns {Object} `obj` after freezing it
+ */
+function deepFreeze(obj) {
+    if (obj === null || typeof obj !== "object") {
+        return obj;
+    }
+
+    Object.keys(obj).map(key => obj[key]).forEach(deepFreeze);
+    return Object.freeze(obj);
+}
+
+module.exports = deepFreeze({
+    env: {},
+    globals: {},
+    rules: {},
+    settings: {},
+    parser: "espree",
+    parserOptions: {
+        ecmaVersion: 5,
+        sourceType: "script",
+        ecmaFeatures: {}
+    }
+});

lib/eslint.js
@@ -14,7 +14,7 @@ const assert = require("assert"),
     eslintScope = require("eslint-scope"),
     levn = require("levn"),
     blankScriptAST = require("../conf/blank-script.json"),
-    DEFAULT_PARSER = require("../conf/eslint-recommended").parser,
+    defaultConfig = require("../conf/default-config-options.js"),
     replacements = require("../conf/replacements.json"),
     CodePathAnalyzer = require("./code-path-analysis/code-path-analyzer"),
     ConfigOps = require("./config/config-ops"),
@@ -453,8 +453,8 @@ function prepareConfig(config) {
     config.globals = config.globals || config.global || {};
     delete config.global;
 
-    const copiedRules = {};
-    let parserOptions = {};
+    const copiedRules = Object.assign({}, defaultConfig.rules);
+    let parserOptions = Object.assign({}, defaultConfig.parserOptions);
 
     if (typeof config.rules === "object") {
         Object.keys(config.rules).forEach(k => {
@@ -484,21 +484,18 @@ function prepareConfig(config) {
 
     const preparedConfig = {
         rules: copiedRules,
-        parser: config.parser || DEFAULT_PARSER,
-        globals: ConfigOps.merge({}, config.globals),
-        env: ConfigOps.merge({}, config.env || {}),
-        settings: ConfigOps.merge({}, config.settings || {}),
+        parser: config.parser || defaultConfig.parser,
+        globals: ConfigOps.merge(defaultConfig.globals, config.globals),
+        env: ConfigOps.merge(defaultConfig.env, config.env || {}),
+        settings: ConfigOps.merge(defaultConfig.settings, config.settings || {}),
         parserOptions: ConfigOps.merge(parserOptions, config.parserOptions || {})
     };
     const isModule = preparedConfig.parserOptions.sourceType === "module";
 
     if (isModule) {
-        if (!preparedConfig.parserOptions.ecmaFeatures) {
-            preparedConfig.parserOptions.ecmaFeatures = {};
-        }
 
         // can't have global return inside of modules
-        preparedConfig.parserOptions.ecmaFeatures.globalReturn = false;
+        preparedConfig.parserOptions.ecmaFeatures = Object.assign({}, preparedConfig.parserOptions.ecmaFeatures, { globalReturn: false });
     }
 
     preparedConfig.parserOptions.ecmaVersion = normalizeEcmaVersion(preparedConfig.parserOptions.ecmaVersion, isModule);
@@ -880,8 +877,8 @@ module.exports = (function() {
             currentConfig = config;
             traverser = new Traverser();
 
-            const ecmaFeatures = currentConfig.parserOptions.ecmaFeatures || {};
-            const ecmaVersion = currentConfig.parserOptions.ecmaVersion || 5;
+            const ecmaFeatures = currentConfig.parserOptions.ecmaFeatures;
+            const ecmaVersion = currentConfig.parserOptions.ecmaVersion;
 
             // gather scope data that may be needed by the rules
             scopeManager = eslintScope.analyze(ast, {
@@ -889,7 +886,7 @@ module.exports = (function() {
                 nodejsScope: ecmaFeatures.globalReturn,
                 impliedStrict: ecmaFeatures.impliedStrict,
                 ecmaVersion,
-                sourceType: currentConfig.parserOptions.sourceType || "script",
+                sourceType: currentConfig.parserOptions.sourceType,
                 fallback: Traverser.getKeys
             });
 
@@ -1183,7 +1180,7 @@ module.exports = (function() {
      * @returns {Object} Object mapping rule IDs to their default configurations
      */
     api.defaults = function() {
-        return require("../conf/eslint-recommended");
+        return defaultConfig;
     };
 
     /**

conf/default-cli-options.js
@@ -12,11 +12,9 @@ module.exports = {
     useEslintrc: true,
     envs: [],
     globals: [],
-    rules: {},
     extensions: [".js"],
     ignore: true,
     ignorePath: null,
-    parser: "", // must be empty
     cache: false,
 
     // in order to honor the cacheFile option if specified

conf/eslint-recommended.js
@@ -10,8 +10,6 @@
 /* eslint-disable sort-keys */
 
 module.exports = {
-    parser: "espree",
-
     rules: {
 
         /* eslint-enable sort-keys */

lib/cli-engine.js
@@ -19,7 +19,7 @@ const fs = require("fs"),
     path = require("path"),
     rules = require("./rules"),
     eslint = require("./eslint"),
-    defaultOptions = require("../conf/cli-options"),
+    defaultOptions = require("../conf/default-cli-options"),
     IgnoredPaths = require("./ignored-paths"),
     Config = require("./config"),
     Plugins = require("./config/plugins"),

lib/config/config-file.js
@@ -23,7 +23,6 @@ const fs = require("fs"),
     stripBom = require("strip-bom"),
     stripComments = require("strip-json-comments"),
     stringify = require("json-stable-stringify"),
-    defaultOptions = require("../../conf/eslint-recommended"),
     requireUncached = require("require-uncached");
 
 const debug = require("debug")("eslint:config-file");
@@ -539,11 +538,6 @@ function load(filePath, applyEnvironments, relativeTo) {
         // ensure plugins are properly loaded first
         if (config.plugins) {
             Plugins.loadAll(config.plugins);
-        }
-
-        // remove parser from config if it is the default parser
-        if (config.parser === defaultOptions.parser) {
-            config.parser = null;
         }
 
         // include full path of parser if present

lib/util/source-code-util.js
@@ -12,7 +12,7 @@
 const CLIEngine = require("../cli-engine"),
     eslint = require("../eslint"),
     globUtil = require("./glob-util"),
-    baseDefaultOptions = require("../../conf/cli-options");
+    baseDefaultOptions = require("../../conf/default-cli-options");
 
 const debug = require("debug")("eslint:source-code-util");
 

tests/fixtures/config-file/js/.eslintrc.parser3.js
@@ -1,4 +1,4 @@
-var defaultOptions = require("../../../../conf/eslint-recommended");
+var defaultOptions = require("../../../../conf/default-config-options");
 
 module.exports = {
     parser: defaultOptions.parser,

commit 720d01512c3d8a92f48ef0a5afe802d1edb6cf25

Fix: don't pass default parserOptions to custom parsers (fixes #8744)
afbea78d4b94839ec6d59d2c98321901aeb76313 accidentally introduced a regression where parsers would get passed additional "default" options even when the user did not specify them. This updates the default parserOptions to prevent any unexpected options from getting passed to parsers.

conf/default-config-options.js
@@ -25,9 +25,5 @@ module.exports = deepFreeze({
     rules: {},
     settings: {},
     parser: "espree",
-    parserOptions: {
-        ecmaVersion: 5,
-        sourceType: "script",
-        ecmaFeatures: {}
-    }
+    parserOptions: {}
 });

lib/linter.js
@@ -455,8 +455,8 @@ function normalizeEcmaVersion(ecmaVersion, isModule) {
  */
 function prepareConfig(config, envContext) {
     config.globals = config.globals || {};
-    const copiedRules = Object.assign({}, defaultConfig.rules);
-    let parserOptions = Object.assign({}, defaultConfig.parserOptions);
+    const copiedRules = {};
+    let parserOptions = {};
 
     if (typeof config.rules === "object") {
         Object.keys(config.rules).forEach(k => {

tests/fixtures/parsers/throws-with-options.js
@@ -1,0 +1,10 @@
+"use strict";
+
+const espree = require("espree");
+
+exports.parse = (sourceText, options) => {
+    if (options.ecmaVersion) {
+        throw new Error("Expected no parserOptions to be used");
+    }
+    return espree.parse(sourceText, options);
+};

tests/lib/eslint.js
@@ -3860,6 +3860,13 @@ describe("eslint", () => {
                 assert.equal(messages[0].message, errorPrefix + require(parser).expectedError);
             });
 
+            it("should not pass any default parserOptions to the parser", () => {
+                const parser = path.join(parserFixtures, "throws-with-options.js");
+
+                const messages = linter.verify(";", { parser }, "filename");
+
+                assert.strictEqual(messages.length, 0);
+            });
         });
     }
 